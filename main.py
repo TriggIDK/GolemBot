8f80106223da35a3cf4bcb2ac10fd47527925470
Trigg <triqerd@gmail.com>
2025-09-07 19:04:06 +0200

    git ignore added

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ import os
 
 load_dotenv
 token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.dm_messages = True
+intents.polls = True
+intents.bans = True
+intents.reactions = True
+intents.dm_reactions = True
+
+bot = commands.Bot(command_prefix='slay', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in {bot.user.name}")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
